aa4f2821e4fbb26bc105dd9ab0f8e3a5460b983e
Aleksey Sadchenko <sadchenk@gmail.com>
2020-11-08 00:55:03 +0300

    change package name

--- clean_code/aircompany/Java/src/main/java/Airport.java
+++ clean_code/aircompany/Java/src/main/java/Airport.java
@@ -1,8 +1,8 @@
-import Planes.experimentalPlane;
+import planes.experimentalPlane;
 import models.MilitaryType;
-import Planes.MilitaryPlane;
-import Planes.PassengerPlane;
-import Planes.Plane;
+import planes.MilitaryPlane;
+import planes.PassengerPlane;
+import planes.Plane;
 
 import java.util.*;
 
